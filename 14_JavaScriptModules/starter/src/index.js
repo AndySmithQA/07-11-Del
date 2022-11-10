945ed19e28c255dde818b426d6a88934c4027bc1
AndySmithQA <andy.smtih@qa.com>
2022-11-10 12:21:58 +0000

    Day 4 Midday

--- 14_JavaScriptModules/starter/src/index.js
+++ 14_JavaScriptModules/starter/src/index.js
@@ -1,31 +1,8 @@
-const nameInput = document.querySelector('[name="name"]');
-const emailInput = document.querySelector('[name="email"]'); 
+import circles from "./circle";
+import * as circlefunct from "./circle";
 
-const formSubmit = event => {
-    event.preventDefault();
-    alert(`The form was submitted.`);
-    for(let i = 0; i < 3;i++) {
-        console.log(event.target[i].value);
-    }
-    
-}
+console.log(circlefunct.area(5))
+console.log(circlefunct.circ(5))
 
-document.querySelector('form').addEventListener('submit', formSubmit);
-
-const validateNameLength = event => {
-    if(event.target.value.length < 2) {
-        alert("Name not long enough");
-        nameInput.focus();
-    }
-}
-
-nameInput.addEventListener('change', validateNameLength);
-
-const validateEmailFormat = event => {
-    if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value))) {
-        alert(`Email address not in a valid format`);
-        emailInput.focus();
-    }
-}
-
-emailInput.addEventListener('change', validateEmailFormat);
+console.log(circles)
+console.log(area())
